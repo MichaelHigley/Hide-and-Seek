c40f2894bf5838c81c92b4b3f8d065e65d3ba64b
Michael <michaelhigley3@gmail.com>
2022-12-22 22:58:55 -0600

    small updates

--- ChessHidenSeek.py
+++ ChessHidenSeek.py
@@ -1,6 +1,7 @@
 # Michael Higley 
 # Started 12/20/2022 -Generated simple game board with 2 players and terrain
-# 12/22/2022 - Added more organizational functions and comments
+# 12/21/2022 - Added more organizational functions and comments
+# 12/22/2022 - Added user input translation to 2d array and organized functions. Need to add movement of hider and seeker next
 # 11x11 grid, one seeker one hider. 3 randomly generated terrain appear every turn on the hiders turn.
 #  if the hider "hides" behind the terrain, the seeker cannot see the hider. Both players can move like a queen in chess and if the hider is caught the games ends.
 # Seeker wins by avoiding for a certain amount of turns
@@ -19,24 +20,32 @@ def createTerrain(numTerrain):
         #print(a,b)
         if (board[a][b] == 0):
             board[a][b] = 3
-            #numTerrain -= 1
         else :
             numTerrain += 1 
 
 #Hiders turn takes input as x and y coordinate
-def hiderTurn(start):
-    move1 = input("Enter coordinate x")
-    move2 = input("Enter coordinate y")
+#Check if end location is viable based on start location
+#Needs to move like a queen in chess (Horizontal, Vertical, Diagonal, and unable to pass through terrain)
+def getHiderMoves(start):
+    move1 = int(input("Enter coordinate x: "))
+    move2 = int(input("Enter coordinate y: "))
     end = (move1,move2)
-    #Check if end location is viable based on start location
-    #Needs to move like a queen in chess (Horizontal, Vertical, Diagonal, and unable to pass through terrain)
+    #Translates the user input to normal x,y coordinates on the board
+    newMoves = inputTranslate(move2,move1)
+    return newMoves
 
-#Returns hider location as x and y
-def getHider():
-    return hiderLocation
-#Sets hider location after changes
-def setHider(x,y):
-    hiderLocation = (x,y)
+#Flips x and y, adds one to y and 11-x to x
+def inputTranslate(y,x):
+    x = x-1
+    y = abs(11 - y)
+    return y,x
+
+def printBoard(board):
+    #Prints board
+    for i in range(len(board)): 
+        for j in range(len(board[i])): 
+            print(board[i][j], end="  ")
+        print() 
 
 #Creates game board array
 rows, cols = (11, 11)
@@ -48,17 +57,17 @@ board[5][5] = 1
 #Generates hider in random place on board
 x = np.random.randint(1,10)
 y = np.random.randint(1,10)
+#Make sure hider can't spawn on seeker spot
+while (x == 5 & y == 5):
+    x = np.random.randint(1,10)
+    y = np.random.randint(1,10)
 board[x][y] = 2
 hiderLocation = (x,y)
-#print(hiderLocation)
+print("Hider starts here: ")
+print(hiderLocation)
 
 #Generates 3 random terrain
 createTerrain(defaultnumTerrain)
 
-#Prints board
-for i in range(len(board)): 
-    for j in range(len(board[i])): 
-        print(board[i][j], end="  ")
-    print() 
-
-
+printBoard(board)
+print(getHiderMoves(hiderLocation))
